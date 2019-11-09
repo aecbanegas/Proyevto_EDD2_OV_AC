8bb937aa9c729ea0f0d782f1adc0db330eaeb867
andresbanegas <44302960+andresbanegas@users.noreply.github.com>
2019-11-09 12:26:11 -0600

    +

--- GestordeArchivos/src/Objetos/ManejoArchivo.java
+++ GestordeArchivos/src/Objetos/ManejoArchivo.java
@@ -5,10 +5,29 @@
  */
 package Objetos;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.io.Serializable;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author MBanegas
  */
-public class ManejoArchivo {
+public class ManejoArchivo implements Serializable{
+    private static final long SerialVersioUID = 608L;
+    RandomAccessFile flujo=null;
+    File archivo=null;
+
+    public ManejoArchivo(File archivo) throws IOException{
+        this.archivo=archivo;
+        if(this.archivo.exists()&&!this.archivo.isFile()){
+            JOptionPane.showConfirmDialog(null, null, archivo.getName()+" no es un Archivo!", 0);
+        }else{
+            flujo=new RandomAccessFile(this.archivo, "rw");
+        }
+        
+    } 
     
 }
